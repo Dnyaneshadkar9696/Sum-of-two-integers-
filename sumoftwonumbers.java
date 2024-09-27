4a537fa8285e9c3f4a625c91b3c410651e402845
DNYANESH RAMDAS ADKAR <dnyaneshadkar@gmail.com>
2024-09-27 11:27:05 +0530

    Add files via upload
    allocate it to another variable and then perform calculations.

---
+++ sumoftwonumbers.java
@@ -0,0 +1,23 @@
+public class sumoftwonumbers {
+    public static void main(String[] args) {
+        int f = getSum();
+        System.out.println(f);
+
+    }
+
+
+
+
+
+    public static int getSum() {
+        int a=3;
+        int b=5;
+        int c;
+        int d;
+        int h;
+        c = a;
+        d = b;
+        h = c+d;
+        return h;
+    }
+}
